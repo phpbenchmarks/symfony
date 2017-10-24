0e2c507799b0e65df5a46d9da44f22a028488c72
Steevan BARBOYON <steevan.barboyon@gmail.com>
2017-10-25 00:02:35 +0200

    Use phpbenchmarks/symfony dependency

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -5,18 +5,31 @@ use Symfony\Component\Config\Loader\LoaderInterface;
 
 class AppKernel extends Kernel
 {
+    /** @var string[] */
+    protected $bundleClasses;
+
+    /**
+     * @param string $environment
+     * @param bool $debug
+     * @param string[] $bundleClasses
+     */
+    public function __construct($environment, $debug, array $bundleClasses = [])
+    {
+        parent::__construct($environment, $debug);
+
+        $this->bundleClasses = $bundleClasses;
+    }
+
     public function registerBundles()
     {
-        $bundles = [
+        $bundles = array(
             new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
-            new Symfony\Bundle\SecurityBundle\SecurityBundle(),
             new Symfony\Bundle\TwigBundle\TwigBundle(),
-            new Symfony\Bundle\MonologBundle\MonologBundle(),
-            new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
-            new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new PHPBenchmarks\BenchmarkBundle\BenchmarkBundle()
-        ];
+        );
+        foreach ($this->bundleClasses as $bundleClass) {
+            $bundles[] = new $bundleClass();
+        }
 
         return $bundles;
     }
@@ -28,16 +41,16 @@ class AppKernel extends Kernel
 
     public function getCacheDir()
     {
-        return dirname(__DIR__).'/var/cache/'.$this->getEnvironment();
+        return dirname(__DIR__) . '/var/cache/' . $this->getEnvironment();
     }
 
     public function getLogDir()
     {
-        return dirname(__DIR__).'/var/logs';
+        return dirname(__DIR__) . '/var/logs';
     }
 
     public function registerContainerConfiguration(LoaderInterface $loader)
     {
-        $loader->load($this->getRootDir().'/config/config_'.$this->getEnvironment().'.yml');
+        $loader->load($this->getRootDir() . '/config/config_' . $this->getEnvironment() . '.yml');
     }
 }
