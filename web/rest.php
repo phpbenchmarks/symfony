e21ff9b0e6aa569ea719e13fd812b62d1b64b14e
Steevan BARBOYON <steevan.barboyon@gmail.com>
2017-10-26 18:21:10 +0200

    Use phpbenchmarks/symfony dependency

---
+++ web/rest.php
@@ -0,0 +1,22 @@
+<?php
+
+// /!\ /!\ /!\ /!\
+// I don't use APC cause bench will sometimes results in a lot of failures
+// /!\ /!\ /!\ /!\
+
+use PhpBenchmarksSymfony\Bundle\RestBundle\RestBundle;
+use Symfony\Component\HttpFoundation\Request;
+
+$loader = require_once __DIR__.'/../app/bootstrap.php.cache';
+
+require_once __DIR__ . '/../app/AppKernel.php';
+require_once __DIR__ . '/../app/AppCache.php';
+
+$kernel = new AppKernel('rest', false, [RestBundle::class]);
+$kernel->loadClassCache();
+$kernel = new AppCache($kernel);
+
+$request = Request::createFromGlobals();
+$response = $kernel->handle($request);
+$response->send();
+$kernel->terminate($request, $response);
