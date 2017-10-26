2671091beaf9d445769f1af53054949d7e3eb867
Steevan BARBOYON <steevan.barboyon@gmail.com>
2017-10-26 18:32:27 +0200

    Use phpbenchmarks/symfony dependency

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -5,24 +5,30 @@ use Symfony\Component\Config\Loader\LoaderInterface;
 
 class AppKernel extends Kernel
 {
+    /** @var string[] */
+    protected $bundleClasses;
+
+    /**
+     * @param string $environment
+     * @param bool $debug
+     * @param string[] $bundleClasses
+     */
+    public function __construct($environment, $debug, array $bundleClasses = [])
+    {
+        parent::__construct($environment, $debug);
+
+        $this->bundleClasses = $bundleClasses;
+    }
+
     public function registerBundles()
     {
         $bundles = array(
             new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
-            new Symfony\Bundle\SecurityBundle\SecurityBundle(),
             new Symfony\Bundle\TwigBundle\TwigBundle(),
-            new Symfony\Bundle\MonologBundle\MonologBundle(),
-            new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
-            new Symfony\Bundle\AsseticBundle\AsseticBundle(),
-            new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new PHPBenchmarks\BenchmarkBundle\BenchmarkBundle()
         );
-
-        if (in_array($this->getEnvironment(), array('dev'))) {
-            $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
-            $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
-            $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
+        foreach ($this->bundleClasses as $bundleClass) {
+            $bundles[] = new $bundleClass();
         }
 
         return $bundles;
@@ -30,6 +36,6 @@ class AppKernel extends Kernel
 
     public function registerContainerConfiguration(LoaderInterface $loader)
     {
-        $loader->load(__DIR__.'/config/config_'.$this->getEnvironment().'.yml');
+        $loader->load($this->getRootDir() . '/config/config_' . $this->getEnvironment() . '.yml');
     }
 }
