cb6ab8536d790464e23bb36601a0492ea77c9030
Steevan BARBOYON <steevan.barboyon@gmail.com>
2017-12-02 17:30:29 +0100

    Update for Symfony Flex

---
+++ init_benchmark.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+function clearCacheAndLogs() {
+    sudo /bin/rm -rf var/cache/*
+    [ "$?" != "0" ] && exit 1
+    sudo /bin/chmod -R 777 var/cache
+    [ "$?" != "0" ] && exit 1
+
+    sudo /bin/rm -rf var/log/*
+    [ "$?" != "0" ] && exit 1
+    sudo /bin/chmod -R 777 var/log
+    [ "$?" != "0" ] && exit 1
+}
+
+function init() {
+    local type=$1
+    if [ "$type" == "1" ]; then
+        env="helloworld";
+    elif [ "$type" == "2" ]; then
+        env="news";
+    else
+        env="rest"
+    fi
+
+    clearCacheAndLogs
+
+    export APP_ENV=$env
+    composer install --no-dev --optimize-autoloader
+    [ "$?" != "0" ] && exit 1
+
+    if [ "$env" == "news" ]; then
+        php app/console assets:install --symlink --env=$env
+        [ "$?" != "0" ] && exit 1
+        php app/console assetic:dump --env=$env
+        [ "$?" != "0" ] && exit 1
+    fi
+
+    clearCacheAndLogs
+
+    return 0
+}
