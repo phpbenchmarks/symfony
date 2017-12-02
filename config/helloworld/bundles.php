2e98a78d412db9199f1b54797eddbe6b77cf3cc6
Steevan BARBOYON <steevan.barboyon@gmail.com>
2017-12-02 17:02:13 +0100

    Symfony 4.0, HelloWorld and Rest benchmarks installation

---
+++ config/helloworld/bundles.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    PhpBenchmarksSymfony\Bundle\HelloWorldBundle\HelloWorldBundle::class => ['all' => true]
+];
