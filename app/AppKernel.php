fcb04014bf1ee30f38869eeacfd04f925b61dded
Steevan BARBOYON <steevan.barboyon@gmail.com>
2017-12-22 02:38:13 +0100

    Create restapi branch

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -5,33 +5,14 @@ use Symfony\Component\Config\Loader\LoaderInterface;
 
 class AppKernel extends Kernel
 {
-    /** @var string[] */
-    protected $bundleClasses;
-
-    /**
-     * @param string $environment
-     * @param bool $debug
-     * @param string[] $bundleClasses
-     */
-    public function __construct($environment, $debug, array $bundleClasses = [])
-    {
-        parent::__construct($environment, $debug);
-
-        $this->bundleClasses = $bundleClasses;
-    }
-
     public function registerBundles()
     {
-        $bundles = array(
+        return [
             new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
             new Symfony\Bundle\TwigBundle\TwigBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-        );
-        foreach ($this->bundleClasses as $bundleClass) {
-            $bundles[] = new $bundleClass();
-        }
-
-        return $bundles;
+            new PhpBenchmarksSymfony\Bundle\RestBundle\RestBundle()
+        ];
     }
 
     public function getRootDir()
@@ -51,6 +32,6 @@ class AppKernel extends Kernel
 
     public function registerContainerConfiguration(LoaderInterface $loader)
     {
-        $loader->load($this->getRootDir() . '/config/config_' . $this->getEnvironment() . '.yml');
+        $loader->load($this->getRootDir() . '/config/config.yml');
     }
 }
--- web/app.php
+++ web/app.php
@@ -4,13 +4,12 @@
 // I don't use APC cause bench will sometimes results in a lot of failures
 // /!\ /!\ /!\ /!\
 
-use PhpBenchmarksSymfony\Bundle\HelloWorldBundle\HelloWorldBundle;
 use Symfony\Component\HttpFoundation\Request;
 
 $loader = require __DIR__.'/../app/autoload.php';
 include_once __DIR__.'/../var/bootstrap.php.cache';
 
-$kernel = new AppKernel('helloworld', false, [HelloWorldBundle::class]);
+$kernel = new AppKernel('prod', false);
 $kernel->loadClassCache();
 
 $request = Request::createFromGlobals();
