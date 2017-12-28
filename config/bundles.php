af9cbdea3326c492a1fbbb181f2b8835e26f88a8
Steevan BARBOYON <steevan.barboyon@gmail.com>
2017-12-28 18:09:28 +0100

    Update phpbenchmarks/symfony dependency

--- config/bundles.php
+++ config/bundles.php
@@ -2,5 +2,5 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-    PhpBenchmarksSymfony\Bundle\HelloWorldBundle\HelloWorldBundle::class => ['all' => true]
+    PhpBenchmarksSymfony\HelloWorldBundle\HelloWorldBundle::class => ['all' => true]
 ];
