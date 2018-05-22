2f88bf5cc06130ed071f650d435a8e25b233d014
Steevan BARBOYON <steevan.barboyon@gmail.com>
2018-05-22 19:58:08 +0200

    Create Hello world benchmark

--- config/bundles.php
+++ config/bundles.php
@@ -2,4 +2,5 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    PhpBenchmarksSymfony\HelloWorldBundle\HelloWorldBundle::class => ['all' => true]
 ];
---
+++ init_benchmark.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+function clearCacheAndLogs() {
+    sudo /bin/rm -rf var/cache/*
+    [ "$?" != "0" ] && exit 1
+    sudo /bin/chmod -R 777 var/cache
+    [ "$?" != "0" ] && exit 1
+
+    sudo /bin/rm -rf var/log/*
+    [ "$?" != "0" ] && exit 1
+    sudo /bin/chmod -R 777 var/log
+    [ "$?" != "0" ] && exit 1
+
+    php bin/console cache:warmup
+}
+
+function init() {
+    export APP_ENV='prod'
+
+    clearCacheAndLogs
+
+    composer install --no-dev --classmap-authoritative
+    [ "$?" != "0" ] && exit 1
+
+    clearCacheAndLogs
+
+    return 0
+}
