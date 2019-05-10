ba318b88a8dab2435d65cf65b037e37ef1414929
Steevan <steevan.barboyon@gmail.com>
2019-05-11 01:09:52 +0200

    Add stats.php

--- public/index.php
+++ public/index.php
@@ -25,3 +25,5 @@ $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
 $kernel->terminate($request, $response);
+
+// require phpbenchmarks stats.php here when needed
