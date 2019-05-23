d8b675279297768128f1e1a9c082dc7746e88cd1
Steevan <steevan.barboyon@gmail.com>
2019-05-23 23:01:01 +0200

    Symfony 4.3.0-BETA2

--- src/Kernel.php
+++ src/Kernel.php
@@ -35,6 +35,11 @@ class Kernel extends BaseKernel
         }
     }
 
+    public function getProjectDir(): string
+    {
+        return \dirname(__DIR__);
+    }
+
     protected function configureContainer(ContainerBuilder $container, LoaderInterface $loader)
     {
         $container->addResource(new FileResource($this->getProjectDir().'/config/bundles.php'));
